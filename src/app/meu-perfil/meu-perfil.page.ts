b621d1e18ce1328ce43b34467a748bd366569abb
23141669+noemialta@users.noreply.github.com <noemi.tavares2@gmail.com>
2019-05-03 21:15:16 -0300

    Adicionando 'alerts' na página do 'Meu perfil'

--- src/app/meu-perfil/meu-perfil.page.ts
+++ src/app/meu-perfil/meu-perfil.page.ts
@@ -15,17 +15,117 @@ export class MeuPerfilPage implements OnInit {
   }
   
 
-  async abrirAlert(){
+  async alterarNomeAlert(){
     let alert = await this.alertCtlr.create({
-      header: "Fulano de Tal",
-      subHeader: "SubHeader",
-      message: "Mensagem do alert",
-      buttons: ["ok"]
+      header: "Nome do usuário:",
+      inputs:[
+        {
+          name: 'nome',
+          type: 'text',
+          placeholder: 'Digite seu novo nome'
+        } ],
+      buttons:[
+        {
+          text: 'Cancelar',
+          cssClass: 'secondary',
+          role: 'cancel',
+          handler: ()=> console.log("aqui vai abrir cancelar a ação de alterar")
+        },
+        {
+          text: 'Alterar',
+          handler: ()=> console.log("Alteração concluída")
+        }
+      ]
     });
     await alert.present();
+  }
 
+  async alterarEmailAlert(){
+    let alert = await this.alertCtlr.create({
+      header: "Email do usuário:",
+      inputs:[
+        {
+          name: 'email',
+          type: 'text',
+          placeholder: 'Digite seu novo e-mail'
+        } ],
+      buttons:[
+        {
+          text: 'Cancelar',
+          cssClass: 'secondary',
+          role: 'cancel',
+          handler: ()=> console.log("aqui vai abrir cancelar a ação de alterar")
+        },
+        {
+          text: 'Alterar',
+          handler: ()=> console.log("Alteração concluída")
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async alterarGeneroAlert(){
+    let alert = await this.alertCtlr.create({
+      header: "Gênero do usuário:",
+      inputs:[
+        {
+          name: 'genero',
+          type: 'text',
+          placeholder: 'Digite seu novo gênero sexual'
+        } ],
+      buttons:[
+        {
+          text: 'Cancelar',
+          cssClass: 'secondary',
+          role: 'cancel',
+          handler: ()=> console.log("aqui vai abrir cancelar a ação de alterar")
+        },
+        {
+          text: 'Alterar',
+          handler: ()=> console.log("Alteração concluída")
+        }
+      ]
+    });
+    await alert.present();
+  }
+  async alterarSenhaAlert(){
+    let alert = await this.alertCtlr.create({
+      header: "Sua senha:",
+      inputs:[
+        {
+          name: 'senha',
+          type: 'password',
+          placeholder: 'Digite sua senha'
+        } ,
+        {
+          name: 'senha1',
+          type: 'password',
+          placeholder: 'Digite sua nova senha'
+        },
+        {
+          name: 'senhanova',
+          type: 'password',
+          placeholder: 'Confirme sua nova senha'
+        }
+      ],
+      buttons:[
+        {
+          text: 'Cancelar',
+          cssClass: 'secondary',
+          role: 'cancel',
+          handler: ()=> console.log("aqui vai abrir cancelar a ação de alterar")
+        },
+        {
+          text: 'Alterar',
+          handler: ()=> console.log("Alteração concluída")
+        }
+      ]
+    });
+    await alert.present();
   }
   
+
 
 }
 
