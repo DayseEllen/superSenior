71ba76cdcd62a19ee4519d83372866f25db98022
DayseEllen <esyadnelle@gmail.com>
2019-05-05 15:51:41 -0300

    restanteDosJogos

--- src/app/tela-arrasta/tela-arrasta.page.ts
+++ src/app/tela-arrasta/tela-arrasta.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tela-arrasta',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TelaArrastaPage implements OnInit {
 
-  constructor() { }
+  constructor(private rota: Router) { }
 
   ngOnInit() {
+  }
+
+  abrirPagina(url:String){
+    this.rota.navigate([url]);
+
   }
 
 }
