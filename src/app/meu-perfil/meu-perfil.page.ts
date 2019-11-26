779a7fff85e86d7354388520d46334dd28b6375b
DayseEllen <esyadnelle@gmail.com>
2019-11-26 21:43:21 -0200

    Continuando a gamificação do jogo das perguntas

--- src/app/meu-perfil/meu-perfil.page.ts
+++ src/app/meu-perfil/meu-perfil.page.ts
@@ -14,10 +14,12 @@ import { BDService } from '../services/bd.service';
 export class MeuPerfilPage implements OnInit {
   usuario: Usuario;
   usuarios: Usuario[]=[];
+  porcentagem: string;
   
   constructor( private autenticacao : Autenticacao, 
     private rota: Router, private bdService: BDService,) {
       this.carregarUsuarios();
+      this.porcentagem='50'
 }
 private async carregarUsuarios(){
   this.usuarios = await this.bdService.listWithUIDs<Usuario>('/usuarios');
@@ -39,13 +41,50 @@ private async carregarUsuarios(){
      
 
   ngOnInit() {
+    
   }
   
-
-
   singOut(){
    this.autenticacao.logout();
+ }
+
+  calcularNivelPergunta(){
+    if (!this.usuario) {
+      this.usuario = <Usuario> {};
+      this.usuario.pontosPerguntas = 50;
+    }
+
+    if(this.usuario.pontosPerguntas >=0 && this.usuario.pontosPerguntas <6){
+        return "Nível 1";
+      }
+      if(this.usuario.pontosPerguntas >=6 && this.usuario.pontosPerguntas <12){
+        return "Nível 2";
+      }
+      if(this.usuario.pontosPerguntas >=12 && this.usuario.pontosPerguntas <18){
+        return "Nível 3";
+      }
+ }
+
+  calcularPorcentagem(){
+   if(this.usuario.pontosPerguntas == 0 || this.usuario.pontosPerguntas == 6 || this.usuario.pontosPerguntas == 12){
+    return 0;
+   }
+   if(this.usuario.pontosPerguntas == 1 || this.usuario.pontosPerguntas == 7 || this.usuario.pontosPerguntas == 13){
+    return 20;
+   }
+   if(this.usuario.pontosPerguntas == 2 || this.usuario.pontosPerguntas == 8 || this.usuario.pontosPerguntas == 14){
+    return '35%';
+  }
+  if(this.usuario.pontosPerguntas == 3 || this.usuario.pontosPerguntas == 9 || this.usuario.pontosPerguntas == 15){
+    return '50%';
+  }
+  if(this.usuario.pontosPerguntas == 4 || this.usuario.pontosPerguntas == 10 || this.usuario.pontosPerguntas == 16){
+    return '70%';
+  }
+  if(this.usuario.pontosPerguntas == 5 || this.usuario.pontosPerguntas == 11 || this.usuario.pontosPerguntas == 17){
+    return '85%';
  }
   
 }
 
+}
