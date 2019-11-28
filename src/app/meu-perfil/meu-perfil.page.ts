8b0ee27f7c8492876b4e61cef35899ad0518ff9c
DayseEllen <esyadnelle@gmail.com>
2019-11-28 17:33:29 -0200

    Finalizando a gamificação do jogo das perguntas

--- src/app/meu-perfil/meu-perfil.page.ts
+++ src/app/meu-perfil/meu-perfil.page.ts
@@ -15,16 +15,19 @@ export class MeuPerfilPage implements OnInit {
   usuario: Usuario;
   usuarios: Usuario[]=[];
   porcentagem: string;
+  nivel: string;
+  pontosP: number;
   
   constructor( private autenticacao : Autenticacao, 
     private rota: Router, private bdService: BDService,) {
       this.carregarUsuarios();
-      this.porcentagem='50'
 }
 private async carregarUsuarios(){
   this.usuarios = await this.bdService.listWithUIDs<Usuario>('/usuarios');
     this.getUser();
-  
+    this.pontosP = this.usuario.pontosPerguntas;
+    this.calcularNivelPergunta();
+    this.calcularPorcentagem();
 }
   private getUser(){
      this.usuario=null;
@@ -48,43 +51,33 @@ private async carregarUsuarios(){
    this.autenticacao.logout();
  }
 
-  calcularNivelPergunta(){
-    if (!this.usuario) {
-      this.usuario = <Usuario> {};
-      this.usuario.pontosPerguntas = 50;
+ calcularPorcentagem(){
+  if(this.nivel == "Nível 1"){
+     this.porcentagem = String(((100 * this.usuario.pontosPerguntas) / 6).toFixed(0));
+  }
+  if(this.nivel == "Nível 2"){
+    this.porcentagem = String(((100 * (this.usuario.pontosPerguntas-6)) / 6).toFixed(0));
+  }
+  if(this.nivel == "Nível 3"){
+    this.porcentagem = String(((100 * (this.usuario.pontosPerguntas-12)) / 6).toFixed(0));
+  }
+}
+
+calcularNivelPergunta(){
+  if (!this.usuario) {
+    this.usuario = <Usuario> {};
+    this.usuario.pontosPerguntas = this.pontosP;
+   }
+
+  if(this.usuario.pontosPerguntas >=0 && this.usuario.pontosPerguntas <6){
+      this.nivel = "Nível 1";
     }
-
-    if(this.usuario.pontosPerguntas >=0 && this.usuario.pontosPerguntas <6){
-        return "Nível 1";
-      }
-      if(this.usuario.pontosPerguntas >=6 && this.usuario.pontosPerguntas <12){
-        return "Nível 2";
-      }
-      if(this.usuario.pontosPerguntas >=12 && this.usuario.pontosPerguntas <18){
-        return "Nível 3";
-      }
- }
-
-  calcularPorcentagem(){
-   if(this.usuario.pontosPerguntas == 0 || this.usuario.pontosPerguntas == 6 || this.usuario.pontosPerguntas == 12){
-    return 0;
-   }
-   if(this.usuario.pontosPerguntas == 1 || this.usuario.pontosPerguntas == 7 || this.usuario.pontosPerguntas == 13){
-    return 20;
-   }
-   if(this.usuario.pontosPerguntas == 2 || this.usuario.pontosPerguntas == 8 || this.usuario.pontosPerguntas == 14){
-    return '35%';
-  }
-  if(this.usuario.pontosPerguntas == 3 || this.usuario.pontosPerguntas == 9 || this.usuario.pontosPerguntas == 15){
-    return '50%';
-  }
-  if(this.usuario.pontosPerguntas == 4 || this.usuario.pontosPerguntas == 10 || this.usuario.pontosPerguntas == 16){
-    return '70%';
-  }
-  if(this.usuario.pontosPerguntas == 5 || this.usuario.pontosPerguntas == 11 || this.usuario.pontosPerguntas == 17){
-    return '85%';
- }
-  
+    if(this.usuario.pontosPerguntas >=6 && this.usuario.pontosPerguntas <12){
+      this.nivel = "Nível 2";
+    }
+    if(this.usuario.pontosPerguntas >=12 && this.usuario.pontosPerguntas <18){
+      this.nivel  = "Nível 3";
+    }
 }
 
 }
